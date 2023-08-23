3aae6a954741d5a4b561b0eb4902f8b23f2b6494
Aditya tiwari <85674450+notstoicc@users.noreply.github.com>
2023-08-23 20:12:10 +0530

    Update data.ts

--- demo/data.ts
+++ demo/data.ts
@@ -1,11 +1,25 @@
 import type { Component } from 'vue'
 
-const video = import.meta.glob('./*.{mp4,jpg,png,gif}', { eager: true, as: 'url' })
+const mediaFiles = import.meta.glob('./*.{mp4,jpg,png,gif}', { eager: true, as: 'url' })
 
 export const demoItems = Array.from(Object.entries(import.meta.glob('./*.md', { eager: true })))
-  .map(([path, page]: any) => ({
-    date: path.slice(2, -3) as string,
-    comp: page.default as Component,
-    video: video[`./${path.slice(2, -3)}.mp4`] as string,
-  }))
-  .sort((a, b) => b.date.localeCompare(a.date))
+  .map(([path, page]: any) => {
+    const date = path.slice(2, -3) as string;
+    const comp = page.default as Component;
+    const mediaPath = `./${path.slice(2, -3)}`;
+    
+    const supportedMediaExtensions = ['mp4', 'jpg', 'png', 'gif'];
+    const media = supportedMediaExtensions.reduce((acc, ext) => {
+      if (mediaFiles[`${mediaPath}.${ext}`]) {
+        acc[ext] = mediaFiles[`${mediaPath}.${ext}`];
+      }
+      return acc;
+    }, {} as Record<string, string>);
+    
+    return {
+      date,
+      comp,
+      media,
+    };
+  })
+  .sort((a, b) => b.date.localeCompare(a.date));
