81f83a2398e174b6c8a591ebd35ff2f0ec881151
Aditya tiwari <85674450+notstoicc@users.noreply.github.com>
2023-08-23 20:16:40 +0530

    Update data.ts 2nd attempt

--- demo/data.ts
+++ demo/data.ts
@@ -2,20 +2,21 @@ import type { Component } from 'vue'
 
 const mediaFiles = import.meta.glob('./*.{mp4,jpg,png,gif}', { eager: true, as: 'url' })
 
-export const demoItems = Array.from(Object.entries(import.meta.glob('./*.md', { eager: true })))
+export const demoItems = Array.from(Object.entries(import.meta.glob('./*.{md,mp4,jpg,png,gif}', { eager: true })))
   .map(([path, page]: any) => {
     const date = path.slice(2, -3) as string;
     const comp = page.default as Component;
     const mediaPath = `./${path.slice(2, -3)}`;
-    
+
     const supportedMediaExtensions = ['mp4', 'jpg', 'png', 'gif'];
-    const media = supportedMediaExtensions.reduce((acc, ext) => {
+    const media: Record<string, string> = {};
+
+    supportedMediaExtensions.forEach(ext => {
       if (mediaFiles[`${mediaPath}.${ext}`]) {
-        acc[ext] = mediaFiles[`${mediaPath}.${ext}`];
+        media[ext] = mediaFiles[`${mediaPath}.${ext}`];
       }
-      return acc;
-    }, {} as Record<string, string>);
-    
+    });
+
     return {
       date,
       comp,
