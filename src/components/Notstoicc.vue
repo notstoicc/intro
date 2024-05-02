3a78a238f998f40225a1a584952039a79a762f83
ノストイック <85674450+notstoicc@users.noreply.github.com>
2024-05-03 01:58:35 +0530

    Create Notstoicc.vue

---
+++ src/components/Notstoicc.vue
@@ -0,0 +1,16 @@
+<template>
+    <p flex="~ gap-2 wrap">
+    <a
+      href="https://www.instagram.com/notstoicc"
+      target="_blank"
+      class="group btn-yellow inline-block"
+    >
+      <div
+        i-ph-lightning-duotone
+        group-hover="i-ph-lightning-fill text-yellow"
+        transition-all duration-200 ease-out
+      />
+      Notstoicc
+    </a>
+  </p>
+</template>
