d66c1a899e3923e4397a8ab3191b8112d9b4df7d
ノストイック <85674450+notstoicc@users.noreply.github.com>
2024-05-03 01:59:09 +0530

    Create Freakyadi.vue

---
+++ src/components/Freakyadi.vue
@@ -0,0 +1,16 @@
+<template>
+    <p flex="~ gap-2 wrap">
+    <a
+      href="https://www.instagram.com/freakyadi"
+      target="_blank"
+      class="group btn-yellow inline-block"
+    >
+      <div
+        i-ph-lightning-duotone
+        group-hover="i-ph-lightning-fill text-yellow"
+        transition-all duration-200 ease-out
+      />
+      Freakyadi
+    </a>
+  </p>
+</template>
