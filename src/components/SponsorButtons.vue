3fb2c84ad84bd3ebd8cc41b1c7224aa6e0295118
ノストイック <85674450+notstoicc@users.noreply.github.com>
2024-05-03 23:36:04 +0530

    Update SponsorButtons.vue

--- src/components/SponsorButtons.vue
+++ src/components/SponsorButtons.vue
@@ -15,8 +15,8 @@
         transition-all duration-200 ease-out
       />
       個人的な選択によるランキング 
-    </a>
-    <RouterLink to="/list" title="Tier List">
+    </a> <br>
+    <RouterLink to="/list" title="Tier List" class="group btn-yellow inline-block">
           <div 
             i-ph-lightning-duotone
             group-hover="i-ph-lightning-fill text-yellow"
