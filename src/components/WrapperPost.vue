3a0b752af4b5d3b83218e6a49ca955c7335a2fc2
ノストイック <85674450+notstoicc@users.noreply.github.com>
2024-05-03 17:57:00 +0530

    Update WrapperPost.vue

--- src/components/WrapperPost.vue
+++ src/components/WrapperPost.vue
@@ -12,9 +12,8 @@ const router = useRouter()
 const route = useRoute()
 const content = ref<HTMLDivElement>()
 
-const base = 'https://antfu.me'
-const tweetUrl = computed(() => `https://twitter.com/intent/tweet?text=${encodeURIComponent(`Reading @antfu7\'s ${base}${route.path}\n\nI think...`)}`)
-const elkUrl = computed(() => `https://elk.zone/intent/post?text=${encodeURIComponent(`Reading @antfu@m.webtoo.ls\'s ${base}${route.path}\n\nI think...`)}`)
+const base = 'https://stoicc.vercel.app'
+const tweetUrl = computed(() => `https://twitter.com/intent/tweet?text=${encodeURIComponent(`Reading @notstoicc's ${base}${route.path}\n\nI think...`)}`)
 
 onMounted(() => {
   const navigate = () => {
