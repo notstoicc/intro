79fe78717cf4e26186191485f31828ee65e40173
ノストイック <85674450+notstoicc@users.noreply.github.com>
2024-05-04 01:04:51 +0530

    Update SpotifyPlaylist.vue

--- src/components/SpotifyPlaylist.vue
+++ src/components/SpotifyPlaylist.vue
@@ -4,11 +4,11 @@
 <template>
   <div slide-enter border="~ base rounded-lg rounded" shadow mt-10 flex="~ col gap-2" of-hidden>
     <div p4 mb--2>
-      <div i-typcn-chevron-right />
-      Visit my <a href="#" target="_blank">Spotify Profile</a>
+      <div i-ic-outline-queue-music />
+      Visit my <a href="https://open.spotify.com/user/317qeapt42gcgh5c4nm5o4zsaerq?si=5gfEWsN1SKuPapX-n2GFBA" target="_blank">Spotify Profile</a>
     </div>
     <div class="embed-spotify-list">
-     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3hRuvtyEcMbsiW5kwmFnhc?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3hRuvtyEcMbsiW5kwmFnhc?utm_source=generator" width="100%" height="152" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
     </div>
   </div>
 </template>
