55a0f7f4b390bed1025e1fbe105d8537c5ad9336
ノストイック <85674450+notstoicc@users.noreply.github.com>
2024-05-04 00:50:21 +0530

    Create SpotifyPlaylist.vue

---
+++ src/components/SpotifyPlaylist.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+</script>
+
+<template>
+  <div slide-enter border="~ base rounded-lg rounded" shadow mt-10 flex="~ col gap-2" of-hidden>
+    <div p4 mb--2>
+      <div i-typcn-chevron-right />
+      Visit my <a href="#" target="_blank">Spotify Profile</a>
+    </div>
+    <div class="embed-spotify-list">
+     <iframe style="border-radius:12px" src="https://open.spotify.com/embed/playlist/3hRuvtyEcMbsiW5kwmFnhc?utm_source=generator&theme=0" width="100%" height="352" frameBorder="0" allowfullscreen="" allow="autoplay; clipboard-write; encrypted-media; fullscreen; picture-in-picture" loading="lazy"></iframe>
+    </div>
+  </div>
+</template>
